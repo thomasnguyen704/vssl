f84d48b2f51e734eabc29ca19568275b417d2a81
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Pro.local>
2020-01-20 10:55:33 -0600

    small adj

--- src/views/items.vue
+++ src/views/items.vue
@@ -6,7 +6,6 @@
       placeholder="Search..."
       autocomplete="off"
     />
-
     <b-table 
       borderless 
       :items="items" 
@@ -14,19 +13,18 @@
       :filter="filter" 
       v-if="filter"
     >
-      <template v-slot:head(result)> 
-        <span></span> 
-      </template>
+      <template v-slot:head(result)> <span></span> </template>
       <template v-slot:cell(result)="data">
         <router-link 
           class="lead text-capitalize" 
           :to=" { name: 'item', params:{ id: data.item.id } }" 
-        > {{data.item.title}} </router-link> <br> 
+        > 
+          {{data.item.title}} 
+        </router-link> <br> 
         <span>{{ data.item.body }}</span> <br>
         <span class="small">{{ data.item.date }}</span>
       </template>
     </b-table>
-
   </div>
 </template>
 
@@ -35,28 +33,23 @@
 
   export default {
     name: 'items',
-
     data() {
       return {
         fields: ['result'],
         filter: ''
       }
     },
-
     async created() {
       await this.loadItems()
     },
-
     methods: {
       ...mapActions(['readItems']),
       async loadItems() {
         await this.readItems()
       }
     },
-
     computed: {
       ...mapState(['items'])
     }
-
   }
 </script>
